553305f9bea8f2e75f2eb8f3cb551e7e0745709b
Damian551 <Bolajiabdul2@gmail.com>
2025-04-02 13:15:37 -0400

    Added the user file

--- src/model/User.js
+++ src/model/User.js
@@ -3,7 +3,8 @@
  * @date: 2025-03-29
  */
 
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,6 +24,18 @@ const userSchema = new mongoose.Schema({
   timestamps: true
 });
 
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Method to compare password
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
 const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+export default User;
